137b96db2ee522a2bd196f0f517135b4e56362ce
ivader8 <iliyadaskalov8@gmail.com>
2019-08-22 00:34:01 +0300

    homepage graphic

--- src/components/CostsTable.js
+++ src/components/CostsTable.js
@@ -6,42 +6,39 @@ import ProductRow from './ProductRow'
 class CostsTable extends Component {
 
     render() {
-      var onCostsTableUpdate = this.props.onCostsTableUpdate;
-      var rowDel = this.props.onRowDel;
-      var filterText = this.props.filterText;
-      var product = this.props.products.map(function(product) {
-        if (product.name.indexOf(filterText) === -1) {
-          return;
-        }
-        return (<ProductRow onCostsTableUpdate={onCostsTableUpdate} product={product} onDelEvent={rowDel.bind(this)} key={product.id}/>)
-      });
-      return (
-        <div>
-  
-  
-        <button type="button" onClick={this.props.onRowAdd} className="btn btn-success pull-right">Add Cost</button>
-        {/* <button type="button" className="btn btn-success pull-right">Reset Table</button> */}
-          <table className="table table-bordered">
-            <thead>
-              <tr>
-                <th>Name</th>
-                <th>price</th>
-                <th>category</th>
-              </tr>
-            </thead>
-  
-            <tbody>
-              {product}
-  
-            </tbody>
-  
-          </table>
-        </div>
-      );
-  
-    }
-  
-  }
-  
+        var onCostsTableUpdate = this.props.onCostsTableUpdate;
+        var rowDel = this.props.onRowDel;
+        var filterText = this.props.filterText;
+        var product = this.props.products.map(function (product) {
+            if (product.name.indexOf(filterText) === -1) {
+                return;
+            }
+            return (< ProductRow onCostsTableUpdate={onCostsTableUpdate}
+                product={product}
+                onDelEvent={rowDel.bind(this)}
+                key={product.id}
+            />)
+        });
+        return (<div >
+            <button type="button"
+                onClick={this.props.onRowAdd}
+                className="btn btn-success pull-right"> Add Cost </button>
+            <table className="table table-bordered" >
+                <thead >
+                    <tr >
+                        <th > Name </th> <th > price </th> <th > category </th> </tr> </thead>
 
-  export default CostsTable;
+                <tbody > {product}
+
+                </tbody>
+
+            </table>
+        </div>
+        );
+
+    }
+
+}
+
+
+export default CostsTable;
